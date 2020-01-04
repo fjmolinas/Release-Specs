75af80b223f388fb54b301bada39a23fcdfba7b8
Francisco Molina <femolina@uc.cl>
2020-01-04 23:07:06 +0100

    04-single-hop-6lowpan-icmp/conftest.py: add run local

--- 04-single-hop-6lowpan-icmp/conftest.py
+++ 04-single-hop-6lowpan-icmp/conftest.py
@@ -1,13 +1,20 @@
-# # content of conftest.py
-# import pytest
+import pytest
 
 
-# def pytest_addoption(parser):
-#     parser.addoption(
-#         "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
-#     )
+def pytest_addoption(parser):
+    parser.addoption(
+        "--local", action="store_true", default=False, help="use local boards"
+    )
 
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--local"):
+        # --local given in cli: all tests can run
+        return
+    local_only = pytest.mark.skip(reason="test cant run on iotlab")
+    for item in items:
+        if "local_only" in item.keywords:
+            item.add_marker(local_only)
 
-# @pytest.fixture
-# def cmdopt(request):
-#     return request.config.getoption("--cmdopt")
+@pytest.fixture
+def local(request):
+    return request.config.getoption("--local")
