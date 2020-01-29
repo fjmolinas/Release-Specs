4b21e89d3c573a1ba9892453a13f0f6ddc5eae96
Francisco Molina <femolina@uc.cl>
2020-01-29 16:21:35 +0100

    06-single-hop-udp: add all tests

--- 06-single-hop-udp/common.py
+++ 06-single-hop-udp/common.py
@@ -7,13 +7,22 @@ def print_results(results):
     print("Average packet losses: {}".format(sum(packet_losses)/len(packet_losses)))
 
 
-def udp_send(source, dest, ip_dest, port, count, payload_size, delay):
-    source.reboot()
-    dest.reboot()
+def udp_send(src, dest, ip_dest, port, count, payload_size, delay):
+    packet_loss = None
+    src_pkt_buf_is_empty = None
+    dest_pkt_buf_is_empty = None
 
-    dest.udp_server_start(port)
-    source.udp_send(ip_dest, port, payload_size, count, delay)
-    packet_loss = dest.udp_server_check_output(count, delay)
-    dest.udp_server_stop()
+    src.reboot()
+    if dest is not None:
+        dest.reboot()
+        dest.udp_server_start(port)
 
-    return packet_loss, source.is_empty(), dest.is_empty()
+    src.udp_send(ip_dest, port, payload_size, count, delay)
+    if dest is not None:
+        packet_loss = dest.udp_server_check_output(count, delay)
+        dest.udp_server_stop()
+        dest_pkt_buf_is_empty = src.is_empty()
+
+    src_pkt_buf_is_empty = src.is_empty()
+
+    return packet_loss, src_pkt_buf_is_empty, dest_pkt_buf_is_empty
