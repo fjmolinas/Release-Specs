b33dc556b2a9b3d86a42b841b7f7fbfd04ffe724
Francisco Molina <femolina@uc.cl>
2020-03-14 14:16:47 +0100

    04-single-hop-6lowpan-icmp/conftest: add --riotbase parameter

--- 04-single-hop-6lowpan-icmp/conftest.py
+++ 04-single-hop-6lowpan-icmp/conftest.py
@@ -1,10 +1,18 @@
+import os
 import pytest
+
+# If no path is given use RIOTBASE environment variable
+RIOTBASE = os.environ.get('RIOTBASE', None)
 
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--local", action="store_true", default=False, help="use local boards"
+        "--local", action="store_true", default=False, help="use local boards",
     )
+    parser.addoption(
+        "--riotbase", default=RIOTBASE, help="RIOTBASE dir",
+    )
+
 
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--local"):
@@ -15,6 +23,12 @@ def pytest_collection_modifyitems(config, items):
         if "local_only" in item.keywords:
             item.add_marker(local_only)
 
+
 @pytest.fixture
 def local(request):
     return request.config.getoption("--local")
+
+
+@pytest.fixture
+def riotbase(request):
+    return os.path.abspath(request.config.getoption("--riotbase"))
