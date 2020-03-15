eaea8ef2882b262374c6aa696834c12bbb09c428
Francisco Molina <femolina@uc.cl>
2020-03-15 12:25:42 +0100

    05-single-hop-route: use common conftest and cleanups

--- 05-single-hop-route/common.py
+++ 05-single-hop-route/common.py
@@ -1,25 +1,9 @@
 from time import sleep
 
-PAYLOAD_SIZE = 1024
-DELAY = 10
-
-
-def print_results(results):
-    packet_losses = [results[i][0] for i in range(len(results))]
-    print("Summary of {packet losses, source pktbuf sanity, \
-            dest pktbuf sanity}:")
-
-    for i in range(len(results)):
-        print("Run {}: {} {} {}".format(
-            i+1, packet_losses[i], results[i][1], results[i][2]))
-
-    print("")
-    print("Average packet losses: {}".format(
-        sum(packet_losses)/len(packet_losses)))
-
 
 def single_hop_run(src, dest, ip_src, ip_dest, src_route, dest_route, rdv,
-                   count, payload_size, delay=10, ping_timeout=10, empty_wait=3):
+                   count, payload_size, delay=10, ping_timeout=10,
+                   empty_wait=3):
     src.reboot()
     dest.reboot()
 
@@ -29,8 +13,8 @@ def single_hop_run(src, dest, ip_src, ip_dest, src_route, dest_route, rdv,
     # get useful information
     src_ifc = src.get_first_iface()
     dest_ifc = dest.get_first_iface()
-    ip_src_ll = dest.get_ip_addr()
-    ip_dest_ll = src.get_ip_addr()
+    ip_src_ll = src.get_ip_addr()
+    ip_dest_ll = dest.get_ip_addr()
 
     # disable router advertisement
     if rdv is False:
@@ -47,9 +31,10 @@ def single_hop_run(src, dest, ip_src, ip_dest, src_route, dest_route, rdv,
     sleep(1)
 
     # add nib routes
-    src.add_nib_route(src_ifc, src_route, ip_src_ll)
-    dest.add_nib_route(dest_ifc, dest_route, ip_dest_ll)
+    src.add_nib_route(src_ifc, src_route, ip_dest_ll)
+    dest.add_nib_route(dest_ifc, dest_route, ip_src_ll)
 
-    packet_loss = src.ping(count, ip_dest.split("/")[0], payload_size, delay, ping_timeout)
+    packet_loss = src.ping(count, ip_dest.split("/")[0], payload_size,
+                           delay, ping_timeout)
 
     return (packet_loss, src.is_empty(), dest.is_empty())
--- 05-single-hop-route/test.py
+++ 05-single-hop-route/test.py
@@ -1,12 +1,10 @@
+import os
+import time
 import pytest
 
-import sys
-import time
-import os
-
-from common import single_hop_run, print_results
-from mixins import RIOTNodeShellIfconfig, RIOTNodeShellPktbuf
-from iotlab import IOTLABNode, IoTLABExperiment
+from common import single_hop_run
+from testutils.mixins import RIOTNodeShellIfconfig, RIOTNodeShellPktbuf
+from testutils.iotlab import IOTLABNode, IoTLABExperiment
 
 
 DEVNULL = open(os.devnull, 'w')
@@ -16,44 +14,52 @@ IOTLAB_EXPERIMENT_DURATION = 120
 class SingleHopNode(RIOTNodeShellIfconfig, RIOTNodeShellPktbuf):
     pass
 
+
 @pytest.fixture
-def nodes(local, request):
+def nodes(local, request, boards):
     nodes = []
-    for board in request.param:
-        env = { 'BOARD': '{}'.format(board) }
-        nodes.append(IOTLABNode(env=env))
-    # Start iot-lab experiment if requested
+    boards_input = boards if boards else request.param
     if local is True:
+        for board in boards_input:
+            env = {'BOARD': '{}'.format(board)}
+            nodes.append(IOTLABNode(env=env))
         yield nodes
     else:
-        exp = IoTLABExperiment(name="RIOT-release-test-06", nodes=nodes)
-        exp.start(duration=IOTLAB_EXPERIMENT_DURATION)
+        for board in boards_input:
+            if IoTLABExperiment.valid_board(board):
+                env = {'BOARD': '{}'.format(board)}
+            else:
+                env = {'IOTLAB_NODE': '{}'.format(board)}
+            nodes.append(IOTLABNode(env=env))
+        exp = IoTLABExperiment(name="RIOT-release-test-04", nodes=nodes)
+        exp.start()
         yield nodes
         exp.stop()
 
+
 @pytest.fixture
-def RIOTNode_factory(nodes):
-    def gnrc_node(i, board_type=None, application_dir="examples/gnrc_networking",
-                  modules='gnrc_pktbuf_cmd', cflags=None, port=None):
-        riotbase = os.environ.get('RIOTBASE', None)
+def RIOTNode_factory(nodes, riotbase):
+    def gnrc_node(i, board_type=None,
+                  application_dir="examples/gnrc_networking",
+                  modules='gnrc_pktbuf_cmd', cflags='', port=None):
         os.chdir(os.path.join(riotbase, application_dir))
         if board_type is not None:
             node = next(n for n in nodes if n.board() == board_type)
         else:
             node = nodes[i]
         node.env['USEMODULE'] = modules
-        if cflags is not None:
-            node.env['CFLAGS'] = cflags
+        node.env['CFLAGS'] = cflags
         if port is not None:
             node.env['PORT'] = port
+        # node._application_directory = os.path.join(riotbase, application_dir)
         node.make_run(['flash'], stdout=DEVNULL, stderr=DEVNULL)
         # Some boards need a delay to start
         time.sleep(3)
         node.start_term()
         return SingleHopNode(node)
-    
+
     yield gnrc_node
-    
+
     for node in nodes:
         node.stop_term()
 
@@ -63,7 +69,8 @@ def RIOTNode_factory(nodes):
                          [pytest.param(['native', 'native'])],
                          indirect=['nodes'])
 def test_task01(nodes, RIOTNode_factory):
-    nodes = [RIOTNode_factory(0, port='tap0'), RIOTNode_factory(1, port='tap1')]
+    nodes = [RIOTNode_factory(0, port='tap0'),
+             RIOTNode_factory(1, port='tap1')]
     for i in range(0, 2):
         packet_loss, buf_source, buf_dest = single_hop_run(nodes[i],
                                                            nodes[i ^ 1],
@@ -77,6 +84,7 @@ def test_task01(nodes, RIOTNode_factory):
         assert(packet_loss < 1)
         assert(buf_source)
         assert(buf_dest)
+
 
 @pytest.mark.parametrize('nodes',
                          [pytest.param(['iotlab-m3', 'iotlab-m3'])],
@@ -97,12 +105,14 @@ def test_task02(nodes, RIOTNode_factory):
         assert(buf_source)
         assert(buf_dest)
 
+
 @pytest.mark.local_only
 @pytest.mark.parametrize('nodes',
                          [pytest.param(['native', 'native'])],
                          indirect=['nodes'])
 def test_task03(nodes, RIOTNode_factory):
-    nodes = [RIOTNode_factory(0, port='tap0'), RIOTNode_factory(1, port='tap1')]
+    nodes = [RIOTNode_factory(0, port='tap0'),
+             RIOTNode_factory(1, port='tap1')]
     for i in range(0, 2):
         packet_loss, buf_source, buf_dest = single_hop_run(nodes[i],
                                                            nodes[i ^ 1],
@@ -116,6 +126,7 @@ def test_task03(nodes, RIOTNode_factory):
         assert(packet_loss < 1)
         assert(buf_source)
         assert(buf_dest)
+
 
 @pytest.mark.parametrize('nodes',
                          [pytest.param(['iotlab-m3', 'iotlab-m3'])],
